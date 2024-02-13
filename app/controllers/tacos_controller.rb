76e48174cfc041ed13d87e147e0a65914f445ab2
Simone D'Luna <simone.dluna@kellogg.northwestern.edu>
2024-02-13 15:17:36 +0000

    Refactoring files

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,6 +1,7 @@
 class TacosController < ApplicationController
 
   def index
+    @fillings = ["Carnitas", "Al Pastor", "Steak", "Fish", "Veggie","Mushroom"]
     # render :template => "tacos/index"
   end
 
